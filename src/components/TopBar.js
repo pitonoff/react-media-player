893e0fc74223d651bd5546c95304cc181de7fde4
Alexander Moshnitsky <pitonthesadboy@gmail.com>
2023-08-07 16:53:46 +0400

    initial commit

---
+++ src/components/TopBar.js
@@ -0,0 +1,9 @@
+const TopBar = () => {
+    return (
+      <div className="top__bar">
+        <p>Two tracks AudioPlayer</p>
+      </div>
+    );
+  };
+  
+  export default TopBar;
---
+++ src/data/tracks.js
@@ -0,0 +1,21 @@
+import vse from './Egor_Letov_-_Vsjo.mp3';
+import seliger from './seliger.mp3';
+
+import egor from './thumbnail.jpg';
+import miha from './thumbnail2.jpg'
+
+export const tracks = [
+   {
+    title: 'Vsio kak u lyudei',
+    src: vse,
+    author: 'Egor Letov',
+    thumbnail: egor,
+   },
+   {
+    title: 'Seliger',
+    src: seliger,
+    author: 'Mikhail Elizarov',
+    thumbnail: miha,
+   }
+];
+
