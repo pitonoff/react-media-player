db915b4fa8e4f1ec8a671b872949fdc5ab99fced
Alexander Moshnitsky <pitonthesadboy@gmail.com>
2023-08-07 17:07:00 +0400

    several css changes

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,7 +1,19 @@
+const Style = {
+    container:{
+        display:"flex",
+        backgroundColor: "000",
+        justifyContent:"center",
+    },
+    text: {
+        color: "#fff",
+        alignSelf:"center",
+    },
+};
+
 const TopBar = () => {
     return (
-      <div className="top__bar">
-        <p>Two tracks AudioPlayer</p>
+      <div style={Style.container}>
+        <p style={Style.text}>Two tracks AudioPlayer</p>
       </div>
     );
   };
